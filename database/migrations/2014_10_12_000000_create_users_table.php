6487abfe13638a0b6636b3c5989f44a8f135d441
Le Toan <toanlhpc02999@fpt.edu.vn>
2024-03-13 12:12:27 +0700

    [Migration] update user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,14 +15,14 @@ return new class extends Migration {
         Schema::create('users', function (Blueprint $table) {
             $table->id()->comment('Mã người dùng');
             $table->string('email')->unique();
-            $table->string('full_name')->nullable(false)->comment('Tên người dùng');
+            $table->string('name')->nullable(false)->comment('Tên người dùng');
             $table->date('birthday')->nullable()->comment('Sinh nhật');
             $table->enum('gender', ['male', 'female'])->nullable()->comment('Giới tính');
             $table->integer('phone')->nullable()->comment('Số điện thoại người dùng');
             $table->string('password')->nullable(false)->comment('Mật khẩu');
-            $table->foreignIdFor(UserAddress::class)->comment('Id địa chỉ');
+            $table->foreignIdFor(UserAddress::class)->nullable()->comment('Id địa chỉ');
             $table->string('avatar')->nullable()->comment('Hình đại diện');
-            $table->foreignIdFor(Shop::class)->comment('Mã nhà bán');
+            $table->foreignIdFor(Shop::class)->nullable()->comment('Mã nhà bán');
             $table->string('verification_code')->nullable()->comment('Mã xác thực');
             $table->string('payment_method')->nullable()->comment('Phương thức thanh toán');
             $table->rememberToken();
