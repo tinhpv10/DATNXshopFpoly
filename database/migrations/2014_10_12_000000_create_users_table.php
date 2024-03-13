638720159213e117a01f4b2e4058ce6a16314bdc
Le Toan <toanlhpc02999@fpt.edu.vn>
2024-03-13 12:00:20 +0700

    [Migration] Sửa cột full_name thành name và thêm nullabel cho 2 cột UserAddress và Shop

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use App\Models\Shop;
 use App\Models\UserAddress;
+
 return new class extends Migration {
     /**
      * Run the migrations.
@@ -13,14 +14,15 @@ return new class extends Migration {
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id()->comment('Mã người dùng');
-            $table->string('full_name')->nullable(false)->comment('Tên người dùng');
+            $table->string('email')->unique();
+            $table->string('name')->nullable(false)->comment('Tên người dùng');
             $table->date('birthday')->nullable()->comment('Sinh nhật');
             $table->enum('gender', ['male', 'female'])->nullable()->comment('Giới tính');
             $table->integer('phone')->nullable()->comment('Số điện thoại người dùng');
             $table->string('password')->nullable(false)->comment('Mật khẩu');
-            $table->foreignIdFor(UserAddress::class)->comment('Id địa chỉ');
+            $table->foreignIdFor(UserAddress::class)->nullable()->comment('Id địa chỉ');
             $table->string('avatar')->nullable()->comment('Hình đại diện');
-            $table->foreignIdFor(Shop::class)->comment('Mã nhà bán');
+            $table->foreignIdFor(Shop::class)->nullable()->comment('Mã nhà bán');
             $table->string('verification_code')->nullable()->comment('Mã xác thực');
             $table->string('payment_method')->nullable()->comment('Phương thức thanh toán');
             $table->rememberToken();
