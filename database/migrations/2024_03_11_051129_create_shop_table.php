c94c8e5c61f9a8f8bbb6bcf9f7217636ff3c5ca5
datttpc02990 <117361057+datttpc02990@users.noreply.github.com>
2024-03-19 11:35:52 +0700

    [Admin] Quản lý, duyệt cửa hàng

--- database/migrations/2024_03_11_051129_create_shop_table.php
+++ database/migrations/2024_03_11_051129_create_shop_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Province;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -18,6 +19,7 @@ return new class extends Migration
             $table->string('email')->nullable(false)->comment('Email cửa hàng');
             $table->integer('phone')->nullable(false)->comment('Số điện thoại cửa hàng');
             $table->string('address')->nullable(false)->comment('Địa chỉ cửa hàng');
+            $table->foreignIdFor(Province::class)->comment('Địa chỉ');
             $table->text('description')->nullable(true)->comment('Mô tả cửa hàng');
             $table->integer('rating')->nullable(true)->comment('Đánh giá cửa hàng');
             $table->tinyInteger('status')->default(1)->comment('Trạng thái cửa hàng');
