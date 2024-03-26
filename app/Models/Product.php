12ec037de7580249c771b33adcae7623c7f3e4a8
TruongVicent <110811587+TruongVicent@users.noreply.github.com>
2024-03-26 13:29:03 +0700

    [Admin]Quản lý Sản phẩm

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,45 @@
 
 namespace App\Models;
 
+use App\Models\Supplier;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'supplier_id',
+        'category_id',
+        'shop_id',
+        'name',
+        'slug',
+        'regular_price',
+        'sale_price',
+        'sku',
+        'Mã SKU',
+        'rating',
+        'view_count',
+        'sold_count',
+        'description',
+        'origin',
+        'meta_title',
+        'meta_description',
+        'meta_keyword',
+    ];
+    public function Supplier(): BelongsTo
+    {
+        return $this->BelongsTo(Supplier::class);
+    }
+    public function shop(): BelongsTo
+    {
+        return $this->BelongsTo(Shop::class);
+    }
+    public function Category(): BelongsTo
+    {
+        return $this->BelongsTo(Category::class);
+    }
+
 }
