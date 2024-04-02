73a12d1e59a7546fcaa381191351039a08738e5a
TruongVicent <110811587+TruongVicent@users.noreply.github.com>
2024-04-02 12:01:51 +0700

    [Admin] update brand để lấy code

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Brand extends Model
 {
@@ -14,4 +15,5 @@ class Brand extends Model
         'description',
         'status'
     ];
+
 }
