1ee0822f6ab5190d659c39ae46c3b1a678baceae
Huu Toan <tienxinhtrai889082@gmail.com>
2024-08-05 14:33:45 +0700

    [Route] Cập nhật Route

--- routes/web.php
+++ routes/web.php
@@ -23,17 +23,6 @@ use App\Http\Controllers\WishListController;
 use Illuminate\Support\Facades\Route;
 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::prefix('/')->group(function () {
     Route::get('/', [HomeController::class, 'home']);
 
