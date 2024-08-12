ac4d7344282a172e7e3db0ff3645aabe66f3f0ef
Minh-Truong <nguyenmingtruong9@gmail.com>
2024-08-12 15:45:45 +0700

    [Model] Các hàm xử lý trạng thái, địa chỉ, ngày , lấy ảnh chính,giá

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Mail;
 use App\Enums\OrderStatus;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Request;
+use Carbon\Carbon;
 
 // Import Log facade
 class Order extends Model
@@ -38,7 +40,10 @@ class Order extends Model
         'cancel_reason',
         'on_hold',
         'is_paid',
-        'day_paid'
+        'day_paid',
+        'lading_code',
+        'check_order_shop',
+        'canceled_by'
     ];
     protected $casts = [
         'status' => OrderStatus::class, // Cast status field to OrderStatus enum
@@ -79,21 +84,102 @@ class Order extends Model
         return $this->hasMany(OrderDetail::class, 'order_id');
     }
 
+    public function shop(){
+        return $this->belongsTo(Shop::class);
+    }
 
 
     protected static function booted()
     {
+        parent::booted();
 
+        // Sự kiện được gọi sau khi model đã được lưu
         static::saved(function ($order) {
-            if ($order->status == OrderStatus::Cancelled) {
-                Mail::to($order->user->email)->send(new OrderCancelledMail($order, $order->cancel_reason));
-            }
-            if ($order->is_paid == 1) {
-                $order->day_paid = now();
-                $order->save();
-            }
+//            if (Request::path() === 'livewire/update') {
+//                if ($order->status == OrderStatus::Cancelled) {
+//                    Mail::to($order->user->email)->send(new OrderCancelledMail($order, $order->cancel_reason));
+//                }
+//            }
+//            if ($order->is_paid == 1) {
+//                $order->day_paid = now();
+//                $order->save();
+//            }
         });
 
     }
+
+
+
+    //self::New => 'Mới',
+    //self::Processing => 'Đang xử lý',
+    //self::Shipped => 'Đã vận chuyển',
+    //self::Delivered => 'Đã giao hàng',
+    //self::Cancelled => 'Đã hủy bỏ',
+    //self::OnHold => 'Đơn tạm giữ',
+    // Các phương thức đếm các số lượng trạng thái đơn hàng
+    // + Chờ xử lý
+    // + Chờ giao hàng
+
+    public static function processingStatus()
+    {
+        return self::where('status', 'Đang xử lý')->count();
+    }
+    // + Vận chuyển
+    public static function shippedStatus()
+    {
+        return self::where('status', 'Đã vận chuyển')->count();
+    }
+    // + Hoàn thành
+    public static function deliveredStatus()
+    {
+        return self::where('status', 'Đã giao hàng')->count();
+    }
+    // + chờ lấy hàng hiện trong shop
+    public static function waitingdelivery(){
+        return self::where('status', 'Chờ lấy hàng')->count();
+    }
+    //
+    public static function NotProcessed(){
+        return self::where('status', 'Chưa xử lý')->count();
+    }
+    public static function processedShop(){
+        return self::where('check_order_shop', 1)->count();
+    }
+
+    public static function getOrderStatusOptions(): array
+    {
+        return array_reduce(OrderStatus::cases(), function ($options, $status) {
+            $options[$status->value] = $status->value;
+            return $options;
+        }, []);
+    }
+    // lấy địa chỉ dưới dạng chuỗi để hiển thị ra shop
+    public function getUserAddressFormattedAttribute()
+    {
+        $address = $this->UserAddress;
+        if (!$address) {
+            return 'Địa chỉ không có sẵn';
+        }
+
+        return sprintf(
+            '%s, %s, %s, %s',
+            $address->address_specific,
+            $address->ward ? $address->ward->name : '',
+            $address->district ? $address->district->name : '',
+            $address->province ? $address->province->name : ''
+        );
+    }
+    // tính số ngày mà shop không vào nhận đơn
+    public function getDaysSinceCustomTimeAttribute()
+    {
+        if ($this->custom_time) {
+            $customTime = Carbon::parse($this->custom_time)->startOfDay();
+            $now = Carbon::now()->startOfDay();
+            return $now->diffInDays($customTime) . ' Ngày';
+        }
+
+        return 'không có';
+    }
+
 
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -152,4 +152,13 @@ class Product extends Model
     {
         return self::where('pause', 1)->count();
     }
+    // lấy ảnh chính
+    public function mainMedia(){
+        return $this->hasOne(ProductMedia::class)->where( 'is_main',1);
+    }
+    // hàm lấy giá
+    public function getPrice(){
+        return $this->sale_price ?? $this->regular_price;
+    }
+
 }
