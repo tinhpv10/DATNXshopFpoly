6608bbad6398a971ff89fe95e16a18c25abb7054
huynh23trh <tranghttpc02468@fpt.edu.vn>
2024-08-23 20:03:42 +0700

    [Admin] Cập nhật chức năng gợi ý bài viết bằng AI

--- app/Filament/Resources/PostResource/Pages/CreatePost.php
+++ app/Filament/Resources/PostResource/Pages/CreatePost.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\PostResource\Pages;
 
 use App\Filament\Resources\PostResource;
+use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreatePost extends CreateRecord
